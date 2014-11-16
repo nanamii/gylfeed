e1d5d398b88e294dd56593866743aa6411338f14
sue <sue@miyu.alphalabs.local>
2014-11-16 22:06:11 +0100

    5th commit

--- Feedhandler.py
+++ Feedhandler.py
@@ -21,15 +21,15 @@ class Feedhandler():
         self.feedList.append(feedparser.parse(self.get_FeedURL(feed_Num)))
 
     def get_FeedURL(self, feed_Num):
-        return self.feedList[feed_Num]["feed"]["title_detail"]["base"]
+        return self.feedList[feed_Num].feed.title_detail.base
 
     def count_Feeds(self):
         return len(self.feedList)
 
     def print_FeedTitles(self):
         for feed in self.feedList:
-            for title_num, entry in enumerate(feed["entries"], start=1):
-                entry_str = entry["title"]
+            for title_num, entry in enumerate(feed.entries, start=1):
+                entry_str = entry.title
                 print("TitleNr. {nr} -> {entry}".format(
                     nr=title_num, entry=entry_str)
                 )
@@ -38,33 +38,32 @@ class Feedhandler():
     def update(self):
         latestFeedList = []
         for feed in self.feedList:
-            latestFeedList.append(feedparser.parse(feed["href"]))
+            latestFeedList.append(feedparser.parse(feed.href))
 
         for feed in latestFeedList:
             for xfeed in self.feedList:
-                if feed["href"] == xfeed["href"]:
+                if feed.href == xfeed.href:
                     self.compare_entries(feed, xfeed)
                 else:
                     print("Keinen übereinstimmenden Feed gefunden")
 
 
     def compare_entries(self, latest_feed, safed_feed):
-        print(latest_feed['href'], safed_feed["href"])
+        print(latest_feed.href, safed_feed.href)
         templist = []
-        tag = False
-        for cur_entry in latest_feed["entries"]:
-                if cur_entry["id"] not in [entry['id'] for entry in safed_feed["entries"]]:
+        for cur_entry in latest_feed.entries:
+                if cur_entry["id"] not in [entry["id"] for entry in safed_feed.entries]:
                     templist.append(cur_entry)
                     print(cur_entry["id"])
-        safed_feed["entries"].extend(templist)
+        safed_feed.entries.extend(templist)
 
 
     def compare_Id(self, id, entry_List):
         for entry in entry_List:
             if id in entry:
-                return false
+                return False
             else:
-                return true
+                return True
 
 
     def save_to_Disk(self):
@@ -73,7 +72,7 @@ class Feedhandler():
                 pickle.dump(self,fp)
                 print("Saving data to disk")
         except IOError as ie:
-            print("Fail to save data" + str(ie))
+            print("Fail to save data {ie}".format(ie=ie))
 
 
 def load_from_Disk():
@@ -82,7 +81,7 @@ def load_from_Disk():
             print("Loading data from disk")
             return pickle.load(fp)
     except IOError as ie:
-            print("Fail to load data" +str(ie))
+            print("Fail to load data {ie}".format(ie=ie))
 
 #fm = Feedhandler()
 #fm.add_Feed("http://rss.sueddeutsche.de/rss/Muenchen")
