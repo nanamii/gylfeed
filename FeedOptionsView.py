fb3195b51ea7961023737af162594b57bfa28165
sue <susanne.kiessling@nullcat.de>
2014-12-16 21:19:56 +0100

    hbox added, align set

--- FeedOptionsView.py
+++ FeedOptionsView.py
@@ -10,25 +10,36 @@ class FeedOptionsView():
         self.grid = Gtk.Grid()
 
         url_label = Gtk.Label("Feed-URL:")
+        url_label.set_alignment(0,0.5)
         url_entry = Gtk.Entry()
 
         naming_label = Gtk.Label("Set a feed name, if you want:")
+        naming_label.set_alignment(0, 0.5)
         naming_entry = Gtk.Entry()
 
         auto_label = Gtk.Label("Update feed automatic")
+        auto_label.set_alignment(0, 0.5)
         auto_switch = Gtk.Switch()
 
         news_label = Gtk.Label("Show only new feed-entries")
+        news_label.set_alignment(0, 0.5)
         news_switch = Gtk.Switch()
 
+        hbox = Gtk.Box()
+        ok_button = Gtk.Button("  OK  ")
+        back_button = Gtk.Button(" Back ")
+        hbox.pack_end(back_button, False, False, 5)
+        hbox.pack_end(ok_button, False, False, 5)
+
+        self.grid.set_border_width(20)
         self.grid.set_column_spacing(30)
         self.grid.set_row_spacing(20)
         self.grid.attach(url_label, 0, 1, 2, 1)
 
         self.grid.insert_row(2)
-        self.grid.attach_next_to(url_entry,url_label,Gtk.PositionType.RIGHT, 4, 1)
+        self.grid.attach_next_to(url_entry,url_label,Gtk.PositionType.RIGHT, 10, 1)
         self.grid.attach(naming_label, 0, 2, 2, 1)
-        self.grid.attach_next_to(naming_entry, naming_label, Gtk.PositionType.RIGHT, 6, 1)
+        self.grid.attach_next_to(naming_entry, naming_label, Gtk.PositionType.RIGHT, 10, 1)
 
         self.grid.insert_row(3)
         self.grid.attach(auto_label,0, 3, 2, 1 )
@@ -36,3 +47,6 @@ class FeedOptionsView():
 
         self.grid.attach(news_label, 0, 4, 2, 1)
         self.grid.attach_next_to(news_switch, news_label, Gtk.PositionType.RIGHT, 1,1)
+
+        self.grid.insert_row(5)
+        self.grid.attach(hbox, 0, 5, 12, 1)
