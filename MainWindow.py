7dfbf6cffcd02ad98135f40d9efe5b9856ca7771
sue <susanne.kiessling@nullcat.de>
2015-01-07 00:25:15 +0100

    search-gui, headerbar

--- MainWindow.py
+++ MainWindow.py
@@ -14,13 +14,16 @@ class MainWindow(Gtk.Window):
         self.set_border_width(10)
         self.set_default_size(800, 600)
 
-        main_window = Gtk.HeaderBar()
-        main_window.set_show_close_button(True)
-        main_window.props.title = "gylfeed"
-        self.set_titlebar(main_window)
+        headerbar = Gtk.HeaderBar()
+        headerbar.set_show_close_button(True)
+        headerbar.props.title = "gylfeed"
+        headerbar.props.subtitle = "the FeeedReader"
+        self.set_titlebar(headerbar)
 
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(box.get_style_context(), "linked")
+
+        searchbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
 
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
         self.add(vbox)
@@ -37,12 +40,30 @@ class MainWindow(Gtk.Window):
         self.button_right.add(Gtk.Arrow(Gtk.ArrowType.RIGHT, Gtk.ShadowType.NONE))
         box.add(self.button_right)
 
-        main_window.pack_start(box)
+        self.button_settings = Gtk.Button.new_from_icon_name('view-sidebar-symbolic', Gtk.IconSize.BUTTON)
+        self.button_settings.connect("clicked", self.open_settingsmenu)
 
+        self.button_search = Gtk.Button.new_from_icon_name('system-search', Gtk.IconSize.BUTTON)
+        self.button_search.connect("clicked", self.manage_searchbar)
+        self.button_search.set_tooltip_text("search for content")
+
+        headerbar.pack_start(box)
+        headerbar.pack_end(self.button_search)
+        headerbar.pack_end(self.button_settings)
+
+        self.searchbar = Gtk.SearchBar()
+        searchentry = Gtk.SearchEntry()
+        self.searchbar.connect_entry(searchentry)
+        self.searchbar.add(searchentry)
+        searchbox.add(self.searchbar)
+        self.searchbar.set_search_mode(False)
+
+        self.menu = Gtk.Menu()
+        menuitem = Gtk.MenuItem(label="update")
+        self.menu.append(menuitem)
+
+        vbox.add(searchbox)
         vbox.add(self.stack)
-
-        self.feed_options = FeedOptionsView()
-        self.stack.add_named(self.feed_options.grid, "feed_options")
 
         self.feedview = Feedview(self)
         self.stack.add_named(self.feedview.box, "listbox")
@@ -56,19 +77,22 @@ class MainWindow(Gtk.Window):
         self.button_left.connect("clicked", self.switch_child)
         self.button_right.connect("clicked", self.switch_child)
 
+        self.feed_options = FeedOptionsView()
+        self.stack.add_named(self.feed_options.grid, "feed_options")
+
 
     def switch_child(self, direction):
         child = {
             self.feed_options.grid:{
                 self.button_left:None,
-                self.button_right:self.entrylist.listbox
+                self.button_right:self.feedview.box,
             },
             self.entrylist.listbox:{
                 self.button_left:self.feedview.box,
                 self.button_right:self.button
             },
             self.feedview.box:{
-                self.button_left:self.feed_options.grid,
+                self.button_left:None,
                 self.button_right:self.entrylist.listbox
             },
             self.button:{
@@ -80,6 +104,17 @@ class MainWindow(Gtk.Window):
         if child is not None:
             self.stack.set_visible_child(child)
 
+
+    def manage_searchbar(self, button_search):
+        if self.searchbar.get_search_mode():
+            self.searchbar.set_search_mode(False)
+        else:
+            self.searchbar.set_search_mode(True)
+
+
+    def open_settingsmenu(self, button_settings):
+        self.menu.popup(None, None, None, None, 0, Gtk.get_current_event_time())
+        self.menu.show_all()
 
 
 
